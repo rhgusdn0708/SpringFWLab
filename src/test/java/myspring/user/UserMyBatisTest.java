61de078f6eb6aa842cff1be6743020f4fb892a8f
rhgusdn0708 <hu070821@gmail.com>
2025-04-28 17:25:20 +0900

    Sqlmap

---
+++ src/test/java/myspring/user/UserMyBatisTest.java
@@ -0,0 +1,38 @@
+package myspring.user;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:spring-beans-mybatis.xml")
+public class UserMyBatisTest {
+	protected static final Logger logger = LogManager.getLogger();
+	
+	@Autowired
+	DataSource dataSource;
+	
+	@Test
+	void connection() {
+		try {
+			Connection connection = dataSource.getConnection();
+			DatabaseMetaData metaData = connection.getMetaData();
+			logger.debug("DB URL = " + metaData.getURL());
+			logger.debug("DB Username = " + metaData.getUserName());
+			logger.info("DB Vendor Name = " + metaData.getDatabaseProductName());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
